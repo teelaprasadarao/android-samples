1e18d522a939ef9e61485c6f56ec130897daef0c
prasadaraoteela <prasadaraoteela@gmail.com>
2022-02-16 18:35:04 +0530

    Created initial project setup

---
+++ app/src/main/java/com/android/samples/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.android.samples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by Prasada Rao on 16/02/22 6:33 PM
+ **/
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
