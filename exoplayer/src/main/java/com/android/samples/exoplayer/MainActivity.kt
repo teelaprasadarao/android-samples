044d3172332a062fa5a0ef053101c19c428b23d1
prasadaraoteela <prasadaraoteela@gmail.com>
2022-02-16 18:40:55 +0530

    Created module for ExoPlayer sample

---
+++ exoplayer/src/main/java/com/android/samples/exoplayer/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.android.samples.exoplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by Prasada Rao on 16/02/22 6:40 PM
+ **/
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
